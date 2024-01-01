d913d0b44e750fe5c0729d8bc680c42ba00182f5
Ahsan90757 <l192702@lhr.nu.edu.pk>
2024-01-02 01:17:29 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Ahsan Mehmood</h1>
+        </header>
+  
+        <section>
+          <h2>About Me</h2>
+          <p>Hi My name is Ahsan</p>
+        </section>
+
+        <footer>
+          <p>Contact: ahsanmehmood90757@gmail.com</p>
+        </footer>
+      </div>
+    );
+  }     
 }
 
 export default App;
